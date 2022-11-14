eb5babd23a37307223933bcf6e7b2c5f689830ce
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-11-14 19:10:07 +0200

    Create code.c

---
+++ Tasks/Task 1/code.c
@@ -0,0 +1,24 @@
+/**********************************************/
+/* Author: Karim Tarek Ibrahim               */
+/********************************************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+    int x;
+    scanf("%d", &x);
+    if(x%2==0)
+        printf("%d is Even", x);
+    else
+        printf("%d is Odd", x);
+
+
+
+
+    return 0;
+}
