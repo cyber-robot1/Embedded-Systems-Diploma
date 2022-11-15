fe539b876a57dc77b27a48830947d5a7fb2769ce
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-11-15 17:52:02 +0200

    Update code.c

--- Codeforwin/Basic C programming exercises/2/code.c
+++ Codeforwin/Basic C programming exercises/2/code.c
@@ -2,6 +2,8 @@
 /*                       Author: Karim Tarek Ibrahim                                */
 /***********************************************************************************/
 
+
+
 /*
 * C program to find sum of two numbers 
 */
