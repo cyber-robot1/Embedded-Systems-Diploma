95f2160f64423e2343100c4ebea355db61131c42
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-11-18 02:34:03 +0200

    Create code.c

---
+++ Codeforwin/If else exercises/2/code.c
@@ -0,0 +1,34 @@
+/*************************************************************************************/
+/*                       Author: Karim Tarek Ibrahim                                */
+/***********************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    // (mx)=> max between three numbers
+    int num1, num2, num3, mx=0;
+
+    // Getting three numbers from user
+    printf("Input num1: ");
+    scanf("%d", &num1);
+    printf("Input num2: ");
+    scanf("%d", &num2);
+    printf("Input num3: ");
+    scanf("%d", &num3);
+
+    // Getting the maximum between two numbers
+    if(num1>num2 && num1>num3)
+        mx=num1;
+    else if(num2>num1 && num2>num3)
+        mx=num2;
+    else
+        mx=num3;
+
+    // Printing the max between the three numbers
+    printf("Maximum is: %d", mx);
+
+    return 0;
+}
