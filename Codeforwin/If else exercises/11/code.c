795cd8745bbb77ac9cddd82fa5a22d5744cbd5b4
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-11-20 19:31:36 +0200

    Create code.c

---
+++ Codeforwin/If else exercises/11/code.c
@@ -0,0 +1,46 @@
+/*************************************************************************************/
+/*                       Author: Karim Tarek Ibrahim                                */
+/***********************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+    unsigned char week;
+
+    // Getting a week number from user
+    printf("Input week number: ");
+    scanf("%d", &week);
+
+    // Printing corresponding day of week name based on its number
+    if((week<1) || (week>7))
+        printf("Invalid input, please enter a digit from 1 to 7 !");
+    else
+    {
+        if(week==1)
+            printf("Monday");
+        
+        else if(week==2)
+            printf("Tuesday");
+        
+        else if(week==3)
+            printf("Wednesday");
+        
+        else if(week==4)
+            printf("Thursday");
+        
+        else if(week==5)
+            printf("Friday");
+        
+        else if(week==6)
+            printf("Saturday");
+        
+        else if(week==7)
+            printf("Sunday");
+    }
+
+    return 0;
+}
