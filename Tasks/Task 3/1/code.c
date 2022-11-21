3d9661061b56c3525d83ad07244541bfc4a19ca0
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-11-21 15:59:45 +0200

    Update code.c

--- Tasks/Task 3/1/code.c
+++ Tasks/Task 3/1/code.c
@@ -12,7 +12,7 @@ int main()
      (x)=> num 1
      (y)=> num 2
     */
-    int x, y, res=0, x_c;
+    int x, y, res=0, x_c, y_c;
 
     // Getting two numbers from user
     printf("Enter first number: ");
@@ -21,18 +21,48 @@ int main()
     scanf("%d", &y);
 
     x_c = x;
+    y_c = y;
 
     // Calculating Quotient without using / operator
     if(y==0)
         printf("%d / %d = Math Error (Undefined Mathematical Operation)", x, y);
     else
     {
-        while(x>=y)
+
+        if(x<0 && y<0)
         {
-            x = x - y;
-            res++;
+            x = -1 * x;
+            y = -1 * y;
+
+            while(x>=y)
+            {
+                x = x - y;
+                res++;
+            }
+            printf("%d / %d = %d", x_c, y_c, res);
         }
-        printf("%d / %d = %d", x_c, y, res);
+
+        else if(x<0)
+        {
+            x = -1 * x;
+            while(x>=y)
+            {
+                x = x - y;
+                res++;
+            }
+            printf("%d / %d = %d", x_c, y, res*-1);
+        }
+
+        else if(y<0)
+        {
+            y = -1 * y;
+            while(x>=y)
+            {
+                x = x - y;
+                res++;
+            }
+            printf("%d / %d = %d", x_c, y_c, res*-1);
+        }
     }
 
     return 0;
