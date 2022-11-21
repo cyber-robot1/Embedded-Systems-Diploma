e9824c92d0679e6bd617591d9a637a7cf1e003b4
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-11-21 14:24:10 +0200

    Create code2.c

---
+++ Sheets/Sheet 1/12/code2.c
@@ -0,0 +1,43 @@
+/*************************************************************************************/
+/*                       Author: Karim Tarek Ibrahim                                */
+/***********************************************************************************/
+
+/*
+ Write a C program to take even numbers from user and print the sum of them, 
+ after each entry if the user enters 2 odd numbers "consecutive" the program print "bye" and stop.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int num, sum=0;
+
+    // (flag)=> counter to count number of odd numbers entered by user
+    unsigned char flag=0;
+
+    while(1)
+    {
+        scanf("%d", &num);
+        if(num%2==0)
+        {
+            sum = sum + num;
+            printf("Sum = %d\n", sum);
+            flag=0;
+        }
+        else
+        {
+            flag++;
+        }
+        if(flag>1)
+        {
+            printf("bye");
+            break;
+        }
+    }
+  
+    return 0;
+}
