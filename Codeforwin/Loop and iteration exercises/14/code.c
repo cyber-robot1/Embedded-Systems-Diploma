b44826fe4d4d7b6e16c4b06ff08c53e9388d2ca2
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-11-26 17:24:13 +0200

    Create code.c

---
+++ Codeforwin/Loop and iteration exercises/14/code.c
@@ -0,0 +1,32 @@
+/*************************************************************************************/
+/*                       Author: Karim Tarek Ibrahim                                */
+/***********************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    /*
+     (sum)=> sum of all digits of the given number
+    */
+    int n, sum=0, digit=0;
+
+    // Getting number from user
+    printf("Input number: ");
+    scanf("%d", &n);
+
+    // Calculating sum of all digits of the given number
+    while(n!=0)
+    {
+        digit=n%10;
+        sum=sum+digit;
+        n=n/10;
+    }
+
+    // Printing sum of all digits of the given number
+    printf("Sum of digits: %d", sum);
+
+    return 0;
+}
