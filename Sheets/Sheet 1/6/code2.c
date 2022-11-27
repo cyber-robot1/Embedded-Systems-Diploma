b514bd731c412628241a865343f8715d936628af
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-11-28 00:24:51 +0200

    Update code2.c

--- Sheets/Sheet 1/6/code2.c
+++ Sheets/Sheet 1/6/code2.c
@@ -2,33 +2,47 @@
 /*                       Author: Karim Tarek Ibrahim                                */
 /***********************************************************************************/
 
-/*
- Getting power of the number without using the built-in function pow()
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
 
-    int base, exponent, power=1;
+    int base, exponent;
+    unsigned long long int power=1;
+    double temp;
 
     // Getting base and exponent from user
-    printf("Enter base: ");
+    printf("Input base: ");
     scanf("%d", &base);
-    printf("Enter exponent: ");
+    printf("Input exponent: ");
     scanf("%d", &exponent);
 
-    // Getting power of the number
-    for(int i=1; i<=exponent; i++)
+    // Checking if the sign of the given exponent (+, -, 0)
+
+    // exponent is positive (+ve)
+    if(exponent>0)
     {
-        power = power * base;
+        for(int i=1; i<=exponent; i++)
+            power=power*base;
+        
+        printf("%d ^ %d = %llu", base, exponent, power);
     }
 
+    // exponent is negative (-ve)
+    else if(exponent<0)
+    {
+        for(int i=1; i<=abs(exponent); i++)
+            power=power*base;
+        
+        temp=1/(long double)power;                       // Type casting for avoiding Integer Division
+        
+        printf("%d ^ %d = %lf",base, exponent, temp);
+    }
 
-    // Printing power of the number
-    printf("Power = %d", power);
+    // exponent is zero
+    else
+        printf("%d ^ 0 = %d", base, 1);
 
     return 0;
 }
