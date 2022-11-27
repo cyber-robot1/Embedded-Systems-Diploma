32b6ceb456e2f2c3533c69574e87cbf2872d6af5
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-11-27 19:13:59 +0200

    Update code.c

--- Tasks/Task 5/4/code.c
+++ Tasks/Task 5/4/code.c
@@ -15,24 +15,56 @@ int firstDigit(int n)
         f_digit=n%10;
         n=n/10;
     }
-  
     return f_digit;
+}
+
+// C function to return the last digit of a given number
+int lastDigit(int n)
+{
+    int l_digit=0;
+
+    l_digit=n%10;
+
+    return l_digit;
+}
+
+// C function to count No. of digits of a given number
+int numOfDigits(int n)
+{
+    int c=0;
+
+    while(n!=0)
+    {
+        n=n/10;
+        c++;
+    }
+
+    return c;
 }
 
 int main()
 {
 
-    // (f_digit)=> first digit of the given number
-    int n, f_digit;
+    /*
+     (f_digit)=> first digit of the given number
+     (l_digit)=> last digit of the given number
+     (n_digit)=> number of digits of the given number
+    */
+    int n, f_digit, l_digit, n_digit;
 
     // Getting number from user
     printf("Enter a number: ");
     scanf("%d", &n);
 
+    // Calculating first, last digits of the given number and its No. of digits
     f_digit=firstDigit(n);
+    l_digit=lastDigit(n);
+    n_digit=numOfDigits(n);
 
-    // Printing first digit of the given number
-    printf("%d", f_digit);
+    // Printing first, last digits of the given number and its No. of digits
+    printf("First digit = %d\n", f_digit);
+    printf("Last digit = %d\n", l_digit);
+    printf("No. of digits = %d\n", n_digit);
 
     return 0;
 }
