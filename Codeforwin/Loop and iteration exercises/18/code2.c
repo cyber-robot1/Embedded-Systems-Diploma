06647f192bfc63936134aab88719e5afc7d627c8
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-11-30 19:20:30 +0200

    Create code2.c

---
+++ Codeforwin/Loop and iteration exercises/18/code2.c
@@ -0,0 +1,5 @@
+  /* ********************************************************************************* */
+ /*                       Author: Karim Tarek Ibrahim                                 */
+/* ********************************************************************************* */
+
+/* Calculating frequency of each digit in the given number by using Frequency Array */
