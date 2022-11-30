e0881fff6dbfc4886c2bcfe7ee32b650b0398231
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-11-30 20:39:53 +0200

    Create code.c

---
+++ Codeforwin/Loop and iteration exercises/20/code.c
@@ -0,0 +1,16 @@
+  /* ********************************************************************************* */
+ /*                       Author: Karim Tarek Ibrahim                                 */
+/* ********************************************************************************* */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{    
+    
+    // Printing ASCII values for all characters
+    for(int i=0; i<=255; i++)
+        printf("ASCII value of character %c = %d\n", i, i);
+
+    return 0;
+}
