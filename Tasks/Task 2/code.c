30418569e1cf7e747e7676f93cbd3360bf1088a4
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2023-09-05 03:20:33 +0300

    Update code.c

--- Tasks/Task 2/code.c
+++ Tasks/Task 2/code.c
@@ -7,18 +7,23 @@
 
 int main()
 {
+
     unsigned int x;
     printf("Enter your mark: ");
     scanf("%d", &x);
     
     if(x>=0 && x<50)
         printf("F");
+	
     else if(x>=50 && x<60)
         printf("D");
+	
     else if(x>=60 && x<70)
         printf("C");
+	
     else if(x>=70 && x<80)
         printf("B");
+	
     else if(x>=80 && x<100)
         printf("A");
 
