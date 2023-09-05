4f375cadfbd6a43a27edc27afe26cb3426d3f3c4
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2023-09-05 03:19:34 +0300

    Create code.c

---
+++ Tasks/Task 2/code.c
@@ -0,0 +1,26 @@
+  /* ********************************************************************************* */
+ /*                       Author: Karim Tarek Ibrahim                                 */
+/* ********************************************************************************* */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    unsigned int x;
+    printf("Enter your mark: ");
+    scanf("%d", &x);
+    
+    if(x>=0 && x<50)
+        printf("F");
+    else if(x>=50 && x<60)
+        printf("D");
+    else if(x>=60 && x<70)
+        printf("C");
+    else if(x>=70 && x<80)
+        printf("B");
+    else if(x>=80 && x<100)
+        printf("A");
+
+    return 0;
+}
