51a0cc60f474e4595922b99cc73dde4f7970fb11
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2023-10-22 21:17:44 +0300

    Update code.c

--- Tasks/Task 4/1/code.c
+++ Tasks/Task 4/1/code.c
@@ -2,31 +2,40 @@
  /*                       Author: Karim Tarek Ibrahim                                 */
 /* ********************************************************************************* */
 
-
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
+
     int n,m,c=0;
     char flagN=0, flagM=0;
-    
-    scanf("%d%d",&n,&m);
-    
+
+    // getting two numbers from user
+    printf("enter first number: ");
+    scanf("%d", &n);
+    printf("enter second number: ");
+    scanf("%d",&m);
+
+    // we can't divide any number by zero
     if(m==0)
-        printf("Math Error\n");
+        printf("Math Error !!\n");
     else
     {
+        // if first number is negative
         if(n<0)
         {
             n*=-1;
             flagN=1;
         }
+
+        // if second number is negative
         if(m<0)
         {
             m*=-1;
             flagM=1;
         }
+
         while(n>=m)
         {
             n-=m;
@@ -38,8 +47,8 @@ int main()
         if(flagM)
             c*=-1;
 
-        printf("%d", c);
+        printf("result = %d", c);
     }
-  
+
     return 0;
 }
